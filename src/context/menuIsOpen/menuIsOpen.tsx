7dff59630888736d2479efec1d992b93d3d7be5a
yurilrodrigues <rodriguesyuri769@gmail.com>
2023-08-29 13:19:13 -0300

    Finalizado

--- src/context/menuIsOpen/menuIsOpen.tsx
+++ src/context/menuIsOpen/menuIsOpen.tsx
@@ -1,16 +1,20 @@
-import { createContext, useContext, useState } from "react";
+import { Dispatch, SetStateAction, createContext, useContext, useState } from "react";
 
+type MenuContextType = {
+  menuIsOpen: boolean;
+  setMenuIsOpen: Dispatch<SetStateAction<boolean>>;
+};
 
-const MenuContext = createContext({ })
+const MenuContext = createContext<MenuContextType | undefined>(undefined);
 
-export const MenuProvider = ({children}: {children: React.ReactNode}) => {
-  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false)
+export const MenuProvider = ({ children }: { children: React.ReactNode }) => {
+  const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false);
 
   return (
-    <MenuContext.Provider value={{menuIsOpen, setMenuIsOpen}}>
+    <MenuContext.Provider value={{ menuIsOpen, setMenuIsOpen }}>
       {children}
     </MenuContext.Provider>
-  )
-}
+  );
+};
 
-export const useMenuContext = () => useContext(MenuContext)
+export const useMenuContext = () => useContext(MenuContext);
